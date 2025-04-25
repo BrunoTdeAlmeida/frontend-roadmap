e63b4ce3f74d3ce5b5b8ccb31fe157ec984c938a
Bruno T de Almeida <brunotorresdealmeida@hotmail.com>
2025-04-25 17:22:44 -0300

    working solution for week 2

--- week2/week-project/my-solution/app.js
+++ week2/week-project/my-solution/app.js
@@ -1,74 +1,76 @@
-let currentPkm = null
-let myPkmns = []
+let currentPkm = null;
+let myPkmns = [];
 
-const pkmnsFromLocalStorage = localStorage.getItem('pokemonTeam')
+const pkmnsFromLocalStorage = localStorage.getItem("pokemonTeam");
 
 if (pkmnsFromLocalStorage) {
-    myPkmns = JSON.parse(pkmnsFromLocalStorage)
+  myPkmns = JSON.parse(pkmnsFromLocalStorage);
 }
 
 const searchPkm = async () => {
-    const pkmNameInput = document.getElementById('pkmInput').value
+  const pkmNameInput = document.getElementById("pkmInput").value;
 
-    // verificando se o input está vazio
-    if (!pkmNameInput) {
-        alert('Choose a pokemon')
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pkmNameInput}`
+    );
+    if (response === Error || "") {
     }
-
-    // buscar pkm digitaod na api
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmNameInput}`)
-
-    currentPkm = await response.json()
-
+    currentPkm = await response.json();
     const pkmHtml = `
         <div>
             <h2>${currentPkm.name}</h2>
             <img src="${currentPkm.sprites.front_default}" alt="${currentPkm.name}" />
             <button onclick="addToTeam()">Add to team</button>
         </div>
-    `
-
-    document.getElementById('pkmResult').innerHTML = pkmHtml
-}
+    `;
+    document.getElementById("pkmResult").innerHTML = pkmHtml;
+  } catch (error) {
+    alert("Enter a valid Pokemon name!");
+  }
+};
 
 const addToTeam = () => {
-    // verificar se a pt está cheia
-    if (myPkmns.length >= 6) {
-        alert('Seu time está cheio.')
-        return
-    }
+  if (myPkmns.length >= 6) {
+    alert("Seu time está cheio.");
+    return;
+  }
 
-    // verificar se o pkm ja esta na pt
-    if (myPkmns.some((pkm) => pkm.name === currentPkm.name)) {
-        alert('Este pokemon já esta no seu time.')
-        return
-    }
+  if (myPkmns.some((pkm) => pkm.name === currentPkm.name)) {
+    alert("Este pokemon já esta no seu time.");
+    return;
+  }
 
-    const pokemonToAdd = {
-        name: currentPkm.name,
-        img: currentPkm.sprites.front_default
-    }
+  const pokemonToAdd = {
+    name: currentPkm.name,
+    img: currentPkm.sprites.front_default,
+  };
 
-    myPkmns.push(pokemonToAdd)
-    localStorage.setItem('pokemonTeam', JSON.stringify(myPkmns))
+  myPkmns.push(pokemonToAdd);
+  localStorage.setItem("pokemonTeam", JSON.stringify(myPkmns));
 
-    displayTeam()
-}
+  displayTeam();
+};
 
 const displayTeam = () => {
-    const teamHTML = myPkmns.map((pkm) => {
-        console.log(pkm)
-        return `
+  const teamHTML = myPkmns
+    .map((pkm) => {
+      return `
             <div>
                 <h3>${pkm.name}</h3>
                 <img src="${pkm.img}" alt="${pkm.name}" />
-                <button>Remove</button>
+                <button onclick="removePkm('${pkm.name}')">Remove</button>
             </div>
-        `
-    }).join('')
+        `;
+    })
+    .join("");
+  document.getElementById("myTeam").innerHTML = teamHTML;
+};
 
-  
-    document.getElementById('myTeam').innerHTML = teamHTML 
-}
+const removePkm = (pokemonName) => {
+  myPkmns = myPkmns.filter((pokemon) => pokemon.name !== pokemonName);
+  localStorage.setItem("pokemonTeam", JSON.stringify(myPkmns));
+  displayTeam();
+};
 
-displayTeam()
+displayTeam();
